ecef707883a37520991be29dff6c43d478c34b03
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2024-03-25 11:22:31 +0100

    feat: add Dice class

---
+++ Dice.cs
@@ -0,0 +1,15 @@
+﻿namespace DiceRollGame
+{
+    public class Dice
+    {
+        private readonly Random _random;
+        private const int SidesCount = 6;
+
+        public int GeneratedNumber { get => _random.Next(1, SidesCount + 1); }
+
+        public Dice(Random random, int sidesCount)
+        {
+            _random = random;
+        }
+    }
+}
